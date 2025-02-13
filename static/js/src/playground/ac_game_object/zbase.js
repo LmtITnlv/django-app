59be5d0384fd6508ed9d7b7e4c6117e94cd951fd
LmtITnlv <1063570011@qq.com>
2025-02-13 14:03:43 +0800

    feat(game): 添加多人模式并优化单机模式
    - 新增 MultiPlayer 消费者类，用于处理多人模式下的 WebSocket 连接
    - 添加 Channels 相关配置，支持 WebSocket
    - 重构 Player 类，支持不同角色（我方玩家、机器人、敌人）
    - 优化游戏地图和技能的渲染逻辑
    - 新增 MultiPlayerSocket 类，用于多人模式下的网络通信
    - 调整游戏动画和渲染流程，提高性能

--- static/js/src/playground/ac_game_object/zbase.js
+++ static/js/src/playground/ac_game_object/zbase.js
@@ -1,67 +1,60 @@
-let AC_GAME_OBJECTS = []; // 储存所有可以“动”的元素的全局数组
+let AC_GAME_OBJECTS = [];
 
-class AcGameObject
-{
-    constructor(hurtable = false) // 构造函数
-    {
-        AC_GAME_OBJECTS.push(this);  // 将这个对象加入到储存动元素的全局数组里
+class AcGameObject {
+    constructor() {
+        AC_GAME_OBJECTS.push(this);
 
-        this.has_call_start = false; // 记录这个对象是否已经调用了start函数
-        this.timedelta = 0; // 当前距离上一帧的时间间隔，相等于时间微分，用来防止因为不同浏览器不同的帧数，物体移动若按帧算会不同，所以要用统一的标准，就要用时间来衡量
-
-        this.hurtable = hurtable; // 决定这个元素能否被碰撞，默认为不能
+        this.has_called_start = false;  // 是否执行过start函数
+        this.timedelta = 0;  // 当前帧距离上一帧的时间间隔
+        this.uuid = this.create_uuid();
     }
 
-    start()
-    {
-        // 只会在第一帧执行一次的过程
+    create_uuid() {
+        let res = "";
+        for (let i = 0; i < 8; i ++ ) {
+            let x = parseInt(Math.floor(Math.random() * 10));  // 返回[0, 1)之间的数
+            res += x;
+        }
+        return res;
     }
 
-    update()
-    {
-        // 每一帧都会执行的过程
+    start() {  // 只会在第一帧执行一次
     }
 
-    destroy()
-    {
+    update() {  // 每一帧均会执行一次
+    }
+
+    on_destroy() {  // 在被销毁前执行一次
+    }
+
+    destroy() {  // 删掉该物体
         this.on_destroy();
-        // 删除这个元素
-        for (let i = 0; i < AC_GAME_OBJECTS.length; ++ i)
-        {
-            if (AC_GAME_OBJECTS[i] === this)
-            {
-                AC_GAME_OBJECTS.splice(i, 1); // 从数组中删除元素的函数splice()
+
+        for (let i = 0; i < AC_GAME_OBJECTS.length; i ++ ) {
+            if (AC_GAME_OBJECTS[i] === this) {
+                AC_GAME_OBJECTS.splice(i, 1);
                 break;
             }
         }
     }
-
-    on_destroy()
-    {
-        // 被删除之前的过程，“临终遗言”
-    }
 }
 
-let last_timestp; // 上一帧的时间
-let AC_GAME_ANIMATION = function(timestp) // timestp 是传入的一个参数，就是当前调用的时间
-{
-    for (let i = 0; i < AC_GAME_OBJECTS.length; ++ i) // 所有动的元素都进行更新。
-    {
+let last_timestamp;
+let AC_GAME_ANIMATION = function(timestamp) {
+    for (let i = 0; i < AC_GAME_OBJECTS.length; i ++ ) {
         let obj = AC_GAME_OBJECTS[i];
-        if (!obj.has_called_start)
-        {
-            obj.start(); // 调用start()
-            obj.has_called_start = true; // 表示已经调用过start()了
-        }
-        else 
-        {
-            obj.timedelta = timestp - last_timestp; // 时间微分
-            obj.update(); // 不断调用
+        if (!obj.has_called_start) {
+            obj.start();
+            obj.has_called_start = true;
+        } else {
+            obj.timedelta = timestamp - last_timestamp;
+            obj.update();
         }
     }
-    last_timestp = timestp; // 进入下一帧时当前时间戳就是这一帧的时间戳
+    last_timestamp = timestamp;
 
-    requestAnimationFrame(AC_GAME_ANIMATION); // 不断递归调用
+    requestAnimationFrame(AC_GAME_ANIMATION);
 }
 
-requestAnimationFrame(AC_GAME_ANIMATION); // JS的API，可以调用1帧里面的函数。(有些浏览器的一秒帧数不一定相等)
+
+requestAnimationFrame(AC_GAME_ANIMATION);
